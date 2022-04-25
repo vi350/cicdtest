737eeef8ca7f7ced74640801229f4cea9e283dda
victor <vi350@inbox.ru>
2022-04-25 12:37:48 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello from gin!",
+		})
+	})
+	err = r.Run(":8080")
+	if err != nil {
+		log.Fatal()
+	}
+}
