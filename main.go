9f88dcd9f31f7246212981e1d5d52aca1c762f1c
victor <vi350@inbox.ru>
2022-04-27 05:24:48 +0300

    testing cd

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "ci cd with docker",
+			"message": "ci cd with docker test",
 		})
 	})
 	err = r.Run(":8080")
